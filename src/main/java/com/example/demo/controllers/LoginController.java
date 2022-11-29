6570373cebee0d7ef1ef575773b6108ae86f345a
Juan Jesús Fernández Rodríguez <alba-juanjesus@hotmail.es>
2022-11-29 12:50:07 +0100

    29/11/22 - 12:50
    Vista del registro +  corrección fallos menores.

--- src/main/java/com/example/demo/controllers/LoginController.java
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -2,15 +2,25 @@ package com.example.demo.controllers;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.example.demo.entity.Usuario;
 
 public class LoginController {
-	
+
 	@GetMapping("/auth/registerForm")
 	public String registerForm(Model model) {
 		model.addAttribute("user", new Usuario());
 		return "registro";
 	}
-	
-	
+
+	@PostMapping("/auth/register")
+	public String register(@ModelAttribute Usuario usuario, RedirectAttributes flash) {
+		//usuarioService.registrar(usuario);
+		flash.addFlashAttribute("succes", "User registered succesfully!");
+		return "redirect:/auth/login";
+	}
 
 }
