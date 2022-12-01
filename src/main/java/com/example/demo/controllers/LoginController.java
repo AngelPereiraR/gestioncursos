3801f18fd92afb1658ac6ec76c9f02b91fff51b7
Ángel Pereira <ampr2003@gmail.com>
2022-12-01 10:29:09 +0100

    Revert "Merge branch 'main' of https://github.com/SpiritVideogames/gestioncursos.git into main"
    This reverts commit 2b595a63892c8dae0a22a3ee5eea7cac66ab6ee4, reversing
    changes made to 9e02f2ddaad642b2bdbeb2224f89194368ea3935.

--- src/main/java/com/example/demo/controllers/LoginController.java
+++ src/main/java/com/example/demo/controllers/LoginController.java
@@ -7,9 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 
 import com.example.demo.entity.Usuario;
 import com.example.demo.services.impl.UserService;
@@ -21,16 +19,8 @@ public class LoginController {
 	@Qualifier("userService")
 	private UserService userService;
 	
-	@GetMapping("/auth/login")
-	public String login(Model model, @RequestParam(name="error", required=false)String error, @RequestParam(name="logout", required=false)String logout) {
-		model.addAttribute("user", new Usuario());
-		model.addAttribute("error", error);
-		model.addAttribute("logout", logout);
-		return "login"	;
-	}
-	
 
-	@GetMapping("/auth/registerform")
+	@GetMapping("/auth/registerForm")
 	public String registerForm(Model model) {
 		model.addAttribute("usuario", new Usuario());
 	
