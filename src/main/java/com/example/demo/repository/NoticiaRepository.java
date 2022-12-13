ac5e9b7fc07048d577bf1ebc894732b887686144
Victor Marquez <marquez.pevic21@cadiz.salesianos.edu>
2022-12-13 14:12:23 +0100

    arreglo de placeholder

--- src/main/java/com/example/demo/repository/NoticiaRepository.java
+++ src/main/java/com/example/demo/repository/NoticiaRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Noticia;
-import com.example.demo.entity.Usuario;
+
 
 @Repository("noticiaRepository")
 public interface NoticiaRepository extends JpaRepository<Noticia, Serializable> {
