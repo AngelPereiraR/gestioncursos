be184911b9dc6eaa9705ca520fd68106ecadf124
Ángel Pereira <ampr2003@gmail.com>
2023-02-01 10:53:57 +0100

    Cambios desplegable

--- src/main/java/com/example/demo/GestionCursosApplication.java
+++ src/main/java/com/example/demo/GestionCursosApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GestionCursosApplication {
+public class GestionCursosApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionCursosApplication.class, args);
 	}
+	
+	@Override
+    public void onStartup(ServletContext servletContext) throws ServletException{
+        super.onStartup(servletContext);
+    }
 
 }
